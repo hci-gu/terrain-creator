c53b4195e90e82f5a05cfc18e38e25dd08a81be8
Malte Henriksson <maltehen132@gmail.com>
2025-05-03 14:57:58 +0200

    added path and component alias

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
 import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
+import path from 'path'
 
 export default defineConfig(() => ({
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, 'src/components'),
       eventsource: 'pocketbase',
     },
   },
